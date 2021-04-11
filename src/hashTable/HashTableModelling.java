31581c58c8b5075516f25013a05be12d784347ef
Emad Chaudhry <emadtc@gmail.com>
2021-04-11 15:27:06 -0400

    Update HashTableModelling.java
    HashTable modelling class has now been updated and shows functionality of all methods

--- src/hashTable/HashTableModelling.java
+++ src/hashTable/HashTableModelling.java
@@ -5,15 +5,20 @@ public class HashTableModelling {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		HashTable test = new HashTable();
-		Key test2 = new Key("Test2");
-		Key test3 = new Key("TestingYfdbd");
-		Key test4 = new Key("fqweg");
-		Key test5 = new Key("Test2");
-		System.out.println(test2.hashMethod());
-		System.out.println(test3.hashMethod());
-		System.out.println(test4.hashMethod());
-		System.out.println(test5.hashMethod());
+		
+		test.insert("Key1", 23);
+		test.insert("/", 100);
+		//Entry testEntry = new Entry(new Key ("Key2") , new Value (25));
+		System.out.println(test.keyPresent("Key1"));
+		System.out.println(test.keyPresent("/"));
+		
+		System.out.println(test.searchValue("/"));
+		
+		test.delete("/");
+		test.dumpMap();
+		test.delete("Key1");
+		System.out.println(test.keyPresent("Key1"));
+		test.dumpMap();
 		
 	}
-
 }
